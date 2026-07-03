5d39aea52a7552c1b56ed1e31d57f6f478e7fdf4
Diego Cardenas <diegosp778@gmail.com>
2026-07-02 21:57:38 -0500

    feat: implement link shortening service and related components

--- src-ts/main.ts
+++
@@ -1,9 +0,0 @@
-import { loadConfig } from "./config.ts";
-import { createApp } from "./container.ts";
-
-const config = loadConfig();
-const { server } = createApp(config);
-
-server.listen(config.port, () =>
-  console.log(`Linker (TS) corriendo en ${config.baseUrl}`)
-);
